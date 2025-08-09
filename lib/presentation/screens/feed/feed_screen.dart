6c9974ef02de3076efe3327a63e0b065c6cd3d8b
Ambrish Tripathi <121783685+AmbrishTripathi6974@users.noreply.github.com>
2025-08-09 14:08:28 +0530

    fix: UI responsiveness

--- lib/presentation/screens/feed/feed_screen.dart
+++ lib/presentation/screens/feed/feed_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:good_space_test/core/utils/helper_function.dart';
 import 'package:good_space_test/presentation/widgets/post_tile.dart';
 import '../../widgets/custom_header.dart';
 
@@ -8,8 +7,6 @@ class FeedScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final size = THelperFunctions.screenSize(context);
-    final theme = Theme.of(context);
     return Scaffold(
       body: CustomScrollView(
         slivers: [
